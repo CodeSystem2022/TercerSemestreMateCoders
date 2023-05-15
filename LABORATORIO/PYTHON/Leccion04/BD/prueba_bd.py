d502e6562904d65b3f33bbce06a94f00389dd421
Eduardo Gomez <edubosterogomez@gmail.com>
2023-05-14 21:53:56 -0300

    Leccion05
    Adjunto el Ejercicio 5 TERMINADO

--- LABORATORIO/PYTHON/Leccion04/BD/prueba_bd.py
+++ LABORATORIO/PYTHON/Leccion04/BD/prueba_bd.py
@@ -1,31 +1,21 @@
 import psycopg2 # Esto es para conectarnos  a Postgre
 
-# creamos un objeto para la conexion
-conexion = psycopg2.connect(
-    user='postgres',
-    password='admin',
-    host='127.0.0.1',
-    port='5432',
-    database='test_bd'
-)
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            entrada = input('Digite los números de registros a eliminar (separados por coma): ')
+            valores = (tuple(entrada.split(',')),) # Esto es una tupla de tuplas
+            # De esta manera ejecutamos la sentencia
+            cursor.execute(sentencia, valores)
+            registros_eliminados = cursor.rowcount
+            print(f'Los registros eliminados son: {registros_eliminados}')
 
-with conexion:
-# recuperamos registros ejecutor
-cursor = conexion.cursor()
-# creamos una sentencia
-sentencia = 'SELECT * FROM persona'
-# De esta manera ejecutamos la sentencia
-cursor.execute(sentencia)
-# Recuperamos todos los registros que seran una lista con tuplas
-registros = cursor.fetchall()
-# mostramos
-print(registros)
-
-# cerramos la conexion y el cursor
-cursor.close()
-conexion.close()
-
-
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
 
 
 
