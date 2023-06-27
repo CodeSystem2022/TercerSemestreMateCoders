743f9a931bbedc2d6998b6ed031a4a3909a2ff37
Martin Torres <103221160+MartinT2222@users.noreply.github.com>
2023-06-27 19:45:39 -0300

    boostrap al registro

--- Cine/vendedor/forms.py
+++ Cine/vendedor/forms.py
@@ -9,9 +9,14 @@ class CompraForm(forms.ModelForm):
         
         
 
-class RegistroForm(forms.ModelForm):
-    contraseña = forms.CharField(widget=forms.PasswordInput())
 
+class RegistroForm(forms.ModelForm):
+    contraseña = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
     class Meta:
         model = Usuario
         fields = ['nombre', 'correo', 'contraseña']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
