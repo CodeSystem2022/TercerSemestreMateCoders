dfa3eb2f4da75e2709f6f06737f76842d929fb66
lautarouqz <lauttaro.urquiza@gmail.com>
2023-06-29 22:09:04 -0300

    Seleccion de butacas terminado - Falta Ticket

--- Cine/vendedor/views.py
+++ Cine/vendedor/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Pelicula, Sala, Funcion
+from .models import Pelicula, Sala, Funcion, Compra
 from .forms import CompraForm
 from decimal import Decimal
+import json
 from django.http import HttpResponse
 
 def home(request):
@@ -41,7 +42,23 @@ def about(request):
 def compra_exitosa(request):
     if request.method == 'POST':
         datos_compra = request.POST.items()
-        entradas =
+        entradas = [tupla for tupla in datos_compra if isinstance(tupla[0],str) and "butaca" in tupla[0]]
+        butacas = {tupla[0].split('_')[1]: bool(tupla[1].lower()) for tupla in entradas}
+        data = json.dumps(butacas)
+        id_compra = request.POST.get("compra_id")
+        compra = Compra.objects.get(id = id_compra)
+        compra.asientos = data
+        funcion = Funcion.objects.get(id=compra.funcion.id)
+        asientos_funcion = funcion.asientos_ocupados
+        for butaca, valor in butacas.items():
+            asientos_funcion[butaca]=valor
+        data = json.dumps(asientos_funcion)
+        funcion.asientos_ocupados = asientos_funcion
+        funcion.save()
+        compra.save()
+
+
+    
 
         return HttpResponse(butacas)
 
